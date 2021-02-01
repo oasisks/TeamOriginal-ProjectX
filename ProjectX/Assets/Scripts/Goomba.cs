792af4ae3cb24ad1b428dafe942776a55ecb4a7a
Oasisks <oasisks2@gmail.com>
2021-01-31 21:01:20 -0500

    Enemy AI Done
    It moves back and forth across a horizontal platform.

---
+++ ProjectX/Assets/Scripts/Goomba.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goomba : MonoBehaviour
+{
+    [SerializeField]
+    private float distance = 5f;
+    [SerializeField]
+    private float speed = 2f;
+
+    private Transform raycastTransform;
+    private Vector3 direction = Vector3.zero;
+    private Vector3 scale = new Vector3(1, 1, 1);
+
+    private void Start()
+    {
+        raycastTransform = transform.GetChild(0).GetComponent<Transform>();
+        Debug.Log(raycastTransform.name);
+        direction.x = 1;
+    }
+
+    private void Update()
+    {
+        GoombaMovement();
+    }
+
+    private void GoombaMovement()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(raycastTransform.transform.position, -Vector2.up, distance);
+        Debug.DrawLine(raycastTransform.transform.position, raycastTransform.transform.position - Vector3.up * 5);
+        if (hit.collider == null)
+        {
+            scale.x = -scale.x;
+            transform.localScale = scale;
+            direction *= -1;
+        }
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
