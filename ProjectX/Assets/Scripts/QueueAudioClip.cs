3e14e861d30a5c4109752f5396f1d459d73ee162
Matthew Caren <matt.t.caren@gmail.com>
2021-02-01 08:11:48 -0800

    test music, add audio queuing script

---
+++ ProjectX/Assets/Scripts/QueueAudioClip.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class QueueAudioClip: MonoBehaviour
+{
+    public AudioSource[] audioSources;
+    private int i;
+    public bool changeClip;
+    private bool didChangeClip;
+
+    void Start() {
+        i = 0;
+    }
+
+    void FixedUpdate() {
+        if(changeClip) {
+            audioSources[i].loop = false;
+            didChangeClip = false;
+        }
+
+        if (!audioSources[i].isPlaying && changeClip && !didChangeClip) {
+            if (i < audioSources.Length) i++;
+            audioSources[i].Play();
+            Debug.Log("Music siwtched to to stage " + i);
+            didChangeClip = true;
+            changeClip = false;
+        }
+    }
+}
