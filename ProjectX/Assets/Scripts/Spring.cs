28eee04aab4f5381d9969d7928d48578dc47ec3b
Oasisks <oasisks2@gmail.com>
2021-02-02 16:16:30 -0500

    The Springs now work

--- ProjectX/Assets/Scripts/Spring.cs
+++ ProjectX/Assets/Scripts/Spring.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Spring : MonoBehaviour
 {
+    [Header("Properties")]
+    [SerializeField] float upwardForce;
     private Animator anim;
 
     private void Start()
@@ -11,14 +13,29 @@ public class Spring : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        Debug.Log(transform.rotation.eulerAngles);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Player")
-            anim.SetBool("playerHasCollided", true);
+        {
+            // play the animation
+            anim.SetTrigger("Bounce");
+            Rigidbody2D playerRB = collision.collider.GetComponent<Rigidbody2D>();
+            Vector3 forceVector = new Vector3(Mathf.Cos((transform.eulerAngles.z + 90f) * Mathf.Deg2Rad), Mathf.Sin((transform.eulerAngles.z + 90f) * Mathf.Deg2Rad), 0f).normalized;
+            Debug.Log(forceVector);
+            playerRB.AddForce(forceVector * upwardForce, ForceMode2D.Impulse);
+        }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        anim.SetBool("playerHasCollided", false);
+        if (collision.collider.tag == "Player")
+        {
+            anim.SetTrigger("Bounce");
+        }
     }
 }
