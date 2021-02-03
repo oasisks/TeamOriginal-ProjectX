eec501a4246718c7c9a716210783d7c83a545ae9
Oasisks <oasisks2@gmail.com>
2021-02-03 16:29:58 -0500

    The goomba can now move properly

--- ProjectX/Assets/Scripts/MainMenu.cs
+++ ProjectX/Assets/Scripts/MainMenu.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void playGame()
+    [SerializeField] Button playButton;
+
+    private void Start()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // add a listener
+        playButton.onClick.AddListener(playGame);
+    }
+    public void playGame()
+    {
+        // TODO: Put the Level selection scene into build.
+        SceneManager.LoadScene("LvlSelect");
     }
 }
