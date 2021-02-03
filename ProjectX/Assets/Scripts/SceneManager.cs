fb79c77d8a53c6138ab83a8348ba5ccfa2fd7c4a
[Zoren] <zsbdeathstar@gmail.com>
2021-02-03 15:53:54 -0500

    Fixed UI, hopefully with scripts this time

---
+++ ProjectX/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+}
