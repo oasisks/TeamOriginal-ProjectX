5f7de132e781bac5c47ebf7fab13457221f43ea4
Oasisks <oasisks2@gmail.com>
2021-02-06 22:24:32 -0500

    Updated Main Menu

---
+++ ProjectX/Assets/Scripts/MainMenuDucky.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuDucky : MonoBehaviour
+{
+    [SerializeField] Transform startLocation;
+    [SerializeField] Transform endLocation;
+    [SerializeField] GameObject duckPrefab;
+
+    private GameObject duck;
+
+    private void Start()
+    {
+        duck = Instantiate(duckPrefab, startLocation.position, Quaternion.identity);
+    }
+
+    private void Update()
+    {
+        MoveDuck();
+    }
+
+    private void MoveDuck()
+    {
+        if (duck != null)
+        {
+            // continuously move towards the end location
+            duck.transform.Translate(Vector3.right * Time.deltaTime);
+            
+            if (duck.transform.position.x == endLocation.position.x)
+            {
+                Destroy(duck);
+            }
+        }
+        else
+        {
+            duck = Instantiate(duckPrefab, startLocation.position);
+        }
+    }
+}
