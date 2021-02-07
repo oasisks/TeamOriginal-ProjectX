7f8ac4defbf6eda59e9c7bd0316878476be9c6e0
Oasisks <oasisks2@gmail.com>
2021-02-06 23:54:48 -0500

    Updated Win Scene and added the congrats png

---
+++ ProjectX/Assets/Scripts/GameFinished.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameFinished : MonoBehaviour
+{
+    [SerializeField] Button goBackToMainMenuButton;
+    [SerializeField] Button quitButton;
+
+    private void Start()
+    {
+        goBackToMainMenuButton.onClick.AddListener(GoToMainMenu);
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    private void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+    private void Quit()
+    {
+        Application.Quit();
+    }
+}
